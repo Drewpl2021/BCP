8df078c4ee5022c8ad2b4a677f5aba345150a557
drewpl2021 <linoplas0@gmail.com>
2025-07-01 11:30:45 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Importar CommonModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'DocumentWeb';
+
+  ngOnInit() {
+    // Lógica para el acordeón
+    const accordions = document.querySelectorAll(".accordion-btn");
+    accordions.forEach((accordion: any) => {
+      accordion.addEventListener("click", function () {
+        // Alternar la visibilidad del panel
+        const panel = accordion.nextElementSibling;
+        panel.classList.toggle("show");
+
+        // Cambiar el texto del botón al abrir/cerrar
+        if (panel.classList.contains("show")) {
+          accordion.innerHTML = accordion.innerHTML.replace("Siguiente", "Cerrar");
+        } else {
+          accordion.innerHTML = accordion.innerHTML.replace("Cerrar", "Siguiente");
+        }
+      });
+    });
+  }
 }
